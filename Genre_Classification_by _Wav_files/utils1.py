49d7982f9b7237a082e808cebcab4a65496ec512
Gina <34631028+GinaCha@users.noreply.github.com>
2019-02-18 03:33:00 +0200

    Add files via upload

---
+++ Genre_Classification_by _Wav_files/utils1.py
@@ -0,0 +1,10 @@
+import os
+
+# Directory where the music dataset is located (GTZAN dataset)
+GENRE_DIR = "data/genres"
+
+# Directory where the test music is located
+TEST_DIR = "data/genre-test"
+
+# All the available genres
+GENRE_LIST = [ "blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"]
