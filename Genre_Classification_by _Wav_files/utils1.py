8c33637b2c3536c005a3b763d555c82aed46ba76
Gina <34631028+GinaCha@users.noreply.github.com>
2019-02-21 11:43:44 +0200

    Update utils1.py

--- Genre_Classification_by _Wav_files/utils1.py
+++ Genre_Classification_by _Wav_files/utils1.py
@@ -1,10 +1,10 @@
 import os
 
 # Directory where the music dataset is located (GTZAN dataset)
-GENRE_DIR = "data/genres"
+GENRE_DIR = "../data/genres"
 
 # Directory where the test music is located
-TEST_DIR = "data/genre-test"
+TEST_DIR = "../wav model/data/genre-test"
 
 # All the available genres
 GENRE_LIST = [ "blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"]
