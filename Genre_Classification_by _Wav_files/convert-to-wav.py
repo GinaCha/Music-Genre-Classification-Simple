0edaaa70cf1b657eccdde4224ed00af14b32102a
Gina <34631028+GinaCha@users.noreply.github.com>
2019-02-21 11:46:10 +0200

    Update convert-to-wav.py

--- Genre_Classification_by _Wav_files/convert-to-wav.py
+++ Genre_Classification_by _Wav_files/convert-to-wav.py
@@ -1,6 +1,8 @@
 import sys
 import os
-#import sox
+import sox
+
+
 op_system = 'WINDOWS'
 #op_system = 'Unix'
 
@@ -10,6 +12,7 @@ genre_dirs = ['data\\genres\\blues','data\\genres\\classical','data\\genres\\cou
 'data\\genres\\disco','data\\genres\\hiphop','data\\genres\\jazz','data\\genres\\metal',
 'data\\genres\\pop','data\\genres\\reggae','data\\genres\\rock'
 ]
+os.chdir("../")
 retval = os.getcwd()
 print ("Current working directory %s" % retval)
 
@@ -30,15 +33,19 @@ for genre_dir in genre_dirs:
 	# loop through each file in current dir
 	for file in os.listdir(k):
 		# SOX
-		os.system("sox " + str(file) + " " + str(file[:-3]) + ".wav")
+		#if having the data folder in current directory and want to remove au files ---also uncomment the "delete .au from current dir" section
+		#os.system("sox " + str(file) + " " + "+str(file[:-3]) + ".wav")
+		
+		os.system("sox " + str(file) + " " + "..\\..\\..\\wav model\\data\\genres\\"+str(file[:-9])+"\\"+str(file[:-3]) + ".wav")
+		#The -v stands for very high quality.
 
-
+	"""
 	# delete .au from current dir
 	if(op_system =='Unix'):
 		os.system("rm *.au")
 	else:
 		os.system("del *.au")
-
+	"""
 
 	# echo contents of current dir
 	print('After conversion:')
